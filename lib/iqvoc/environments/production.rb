868ca448edc7a7c10e1eef25ca67992be4abc34f
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2020-04-21 09:33:12 +0100

    Force SSL

--- lib/iqvoc/environments/production.rb
+++ lib/iqvoc/environments/production.rb
@@ -45,7 +45,7 @@ module Iqvoc::Environments
     # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
     # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-    # config.force_ssl = true
+    config.force_ssl = true
 
     # Decrease the log volume.
     config.log_level = :info
