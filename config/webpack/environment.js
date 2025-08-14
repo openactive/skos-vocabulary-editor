0ca9f26297616ad8f2969eabeacd70b6a9ec92c3
Darren Temple <darren.temple@theodi.org>
2025-08-14 11:47:30 +0100

    - Further tweaks following errors

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
+const path = require('path')
+
+const babelLoader = environment.loaders.get('babel')
+
+// Explicitly include app/javascript and jqtree module for babel transpiling
+babelLoader.include = [
+  path.resolve(__dirname, '../../app/javascript'),
+  path.resolve(__dirname, '../../node_modules/jqtree'),
+]
 
 module.exports = environment
